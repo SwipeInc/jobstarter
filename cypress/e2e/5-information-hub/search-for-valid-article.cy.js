8bb051303e43d6f0e0762b6a29c2ba18ced39db2
Rasool <rasool@swipeix.com>
2024-02-27 16:29:04 +0200

    Added saving article test

--- cypress/e2e/5-information-hub/search-for-valid-article.cy.js
+++ cypress/e2e/5-information-hub/search-for-valid-article.cy.js
@@ -4,15 +4,28 @@ beforeEach(() => {
     cy.LoginWithUI();
 });
 
-it('find no content in info hub', () => {
+it('Search for article and click on it', () => {
+    const validInfoSearchTerm = Cypress.env('validInfoSearchTerm');
+
     //search for an invalid string and find no content
-    cy.searchInfoHub(Cypress.env('validInfoSearchTerm'));
+    cy.searchInfoHub(validInfoSearchTerm);
     cy.wait(1000);
     cy.contains("Eish, we can't find anything... ").should('not.exist');
 
     //search for an item and validate that you land on a detail page when clicked
     cy.get('.min-h-0.px-6')
-        .contains(Cypress.env('validInfoSearchTerm'))
+        .contains(validInfoSearchTerm)
         .click();  
-        cy.url().should('include', '/information-hub/articledetails/');
+
+    //like article check it was saved then unlike it
+    cy.url().should('include', '/information-hub/articledetails/');
+    cy.wait(1000);
+    cy.get('.w-40 > .flex').click();
+    cy.contains("Go back").click();
+    cy.contains("Take me there").click();
+    cy.get('.relative > :nth-child(1) > .w-full').should('be.visible');
+    cy.get('.pt-3 > .flex').click();
+    cy.get('.relative > :nth-child(1) > .w-full').should('not.exist');
 });
+
+
